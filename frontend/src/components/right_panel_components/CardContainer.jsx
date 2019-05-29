e8a38c934870ee39523c6d4237b3202fabf45ae8
UsuallyChris <schoonc@gmail.com>
2019-05-29 13:31:06 -0700

    CardContainer grabs bill info from backend and renders out a card for each bill

--- frontend/src/components/right_panel_components/CardContainer.jsx
+++ frontend/src/components/right_panel_components/CardContainer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 // COMPONENT IMPORTS
 import Card from './Card';
@@ -9,12 +10,30 @@ class CardContainer extends Component {
     this.state = {
       bills: []
     }
+
+    this.getBills = this.getBills.bind(this);
+  }
+
+  getBills() {
+    axios.get('http://localhost:5000/bills')
+      .then(res => this.setState({ bills: res.data }));
+  };
+
+  componentDidMount() {
+    this.getBills();
   }
 
   render() {
     return(
       <div className="card-container">
-        <Card />
+        {this.state.bills.map(bill => (
+          <Card
+            name={bill.name}
+            date_due={bill.date_due}
+            amount={bill.amount}
+            type={bill.type}
+          />
+        ))}
       </div>
     )
   };
