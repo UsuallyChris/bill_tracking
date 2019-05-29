a95a4782f777f0c5cadd86643b20d2d2805b3f2e
UsuallyChris <schoonc@gmail.com>
2019-05-29 13:21:18 -0700

    Card takes correct props

--- frontend/src/components/right_panel_components/Card.jsx
+++ frontend/src/components/right_panel_components/Card.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Card(props) {
   return(
-    <h3>Card</h3>
+    <div className="card">
+      <h3>{props.name}</h3>
+      <hr/>
+      <div className="card-details">
+        <p>Date Due: {props.date_due}</p>
+        <p>Amount Due: {props.amount}</p>
+        <p>Type: {props.type}</p>
+      </div>
+    </div>
   );
 };
 
