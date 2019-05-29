ac3598625713407fabeb4b265ed0b1f5c6a01bbf
UsuallyChris <schoonc@gmail.com>
2019-05-29 11:53:08 -0700

    Created logout component and added to LeftPanel

--- frontend/src/components/LeftPanel.jsx
+++ frontend/src/components/LeftPanel.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// COMPONENT IMPORTS
+import Logout from './left_panel_components/Logout';
+
 function LeftPanel() {
   return(
     <div className="left-container">
       <h2>Left Panel</h2>
+      <Logout />
     </div>
   );
 };
---
+++ frontend/src/components/left_panel_components/Logout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Logout() {
+  return(
+    <div className="logout">
+      <h3>UsuallyChris</h3>
+      <h3>Logout</h3>
+    </div>
+  );
+};
+
+export default Logout;
