c7ae0311e05e4d6d97a86216836c21fb1a2e4347
UsuallyChris <schoonc@gmail.com>
2019-06-01 18:41:17 -0700

    Defined viewset and urls for Bills app

--- backend/bills/views.py
+++ backend/bills/views.py
@@ -1,2 +1,13 @@
 """ view definitions for bills app """
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Bill
+from .serializers import BillSerializer
+
+
+class BillViewSet(viewsets.ModelViewSet):
+    """ ViewSet for Bill model """
+    queryset = Bill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = BillSerializer
